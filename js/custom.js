81437bd0085e54fc126d6da911575f371c2824dc
Andrew <bloyce1990@gmail.com>
2019-09-22 21:29:20 +1000

    testimonials + mobile menu refinements

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+// Homepage
+
+$(document).ready(function(){
+	$('.testimonials-wrapper').slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 5000
+	});
+});
